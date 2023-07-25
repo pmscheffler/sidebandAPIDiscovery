b2aa5f9f5acacb1c2b03f1bb93e0110ca12480fc
Peter Scheffler <p.scheffler@f5.com>
2023-07-25 15:23:25 -0400

    fixes

--- testing/bigrun.sh
+++ testing/bigrun.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
 
-for i in {1..24}; do ./testbatch.sh {$1}_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
-for i in {1..24}; do ./testbatch.sh {$1}_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
-for i in {1..24}; do ./testbatch.sh {$1}_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
-for i in {1..24}; do ./testbatch.sh {$1}_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
-for i in {1..24}; do ./testbatch.sh {$1}_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
-for i in {1..24}; do ./testbatch.sh {$1}_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
-for i in {1..24}; do ./testbatch.sh {$1}_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
-for i in {1..24}; do ./testbatch.sh {$1}_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
-for i in {1..24}; do ./testbatch.sh {$1}_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
-for i in {1..24}; do ./testbatch.sh {$1}_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh {$1}0_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh {$1}1_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh {$1}2_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh {$1}3_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh {$1}4_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh {$1}5_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh {$1}6_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh {$1}7_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh {$1}8_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh {$1}9_${i}@testing.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
 
 
 
