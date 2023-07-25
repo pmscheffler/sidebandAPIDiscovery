7afb813ab35bca640b59d37706a10a2db15d2d91
Peter Scheffler <p.scheffler@f5.com>
2023-07-25 15:18:31 -0400

    Added testing runs Added framework for AS3 (ChatGPT helped) Updates to apilogger to minimize noise

--- logger/apilogger.js
+++ logger/apilogger.js
@@ -12,13 +12,13 @@ const logger = winston.createLogger({
     format: winston.format.json(),
     defaultMeta: { service: 'user-service' },
     transports: [
-      //
-      // - Write all logs with importance level of `error` or less to `error.log`
-      // - Write all logs with importance level of `info` or less to `combined.log`
-      //
-      new winston.transports.Console(),
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        //   new winston.transports.Console(),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
     ],    
 });
 
@@ -189,8 +189,8 @@ function sendRequest(reqIn, outIdx) {
     } else {
         headers["x-forwarded-for"] = reqIn.clientip + "," + headers["x-forwarded-for"];
     }
-    console.log(`reqIn: ${util.inspect(reqIn)}`);
-    console.log(`X-Forwarded-For: ${headers["x-forwarded-for"]}`)
+    // console.log(`reqIn: ${util.inspect(reqIn)}`);
+    // console.log(`X-Forwarded-For: ${headers["x-forwarded-for"]}`)
     logger.verbose(`X-Forwarded-For: ${headers["x-forwarded-for"]}`);
 
     // find the proper XC LB to send the mimicked request to
---
+++ testing/bigrun.sh
@@ -0,0 +1,15 @@
+for i in {1..24}; do ./testbatch.sh testaws_loop70_${i}@aws_udf.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh testaws_loop71_${i}@aws_udf.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh testaws_loop72_${i}@aws_udf.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh testaws_loop73_${i}@aws_udf.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh testaws_loop74_${i}@aws_udf.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh testaws_loop75_${i}@aws_udf.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh testaws_loop77_${i}@aws_udf.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh testaws_loop77_${i}@aws_udf.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh testaws_loop78_${i}@aws_udf.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+for i in {1..24}; do ./testbatch.sh testaws_loop79_${i}@aws_udf.com 63469153-8479-4340-87e3-c7bc1ae1cd46.access.udf.f5.com ${i}; sleep 10; done
+
+
+
+
+
